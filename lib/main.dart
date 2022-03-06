0bfe3b9fa4e764c1f7e996dc7779bb6d09eecf0d
unknown <shubham.tade@gmail.com>
2022-03-05 17:25:22 -0800

    final Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://images.unsplash.com/photo-1646237355399-d3c9be1e056d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+            image: AssetImage('images/new.jpg'),
           ),
         ),
       ),
